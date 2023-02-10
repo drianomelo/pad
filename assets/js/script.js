530ffebe4a5191007dfd86bdee2cebe8ee84ed75
Adriano <adrianosenny@gmail.com>
2023-02-10 11:43:15 -0300

    Criando principal funcionalidade do projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
+function tocaSom(idAudio) {
+  document.querySelector(idAudio).play();
+}
 
+const listaDeTeclas = document.querySelectorAll(".main__item");
+
+let cont = 0;
+
+while (cont < listaDeTeclas.length) {
+  const tecla = listaDeTeclas[cont];
+  const somTecla = tecla.classList[1];
+  const idAudio = `#som_${somTecla}`;
+
+  tecla.onclick = () => {
+    tocaSom(idAudio);
+  };
+
+  cont++;
+}
